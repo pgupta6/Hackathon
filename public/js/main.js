37c2ff4aa2ccfa5a12b2acbb18d6ee3124e6812f
unknown <wilsamue@WILSAMUE-WS.cisco.com>
2014-09-18 13:27:33 -0700

    weather service api integration

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
+var url = 'http://api.wunderground.com/api/1df22023c66b0e1d/forecast10day/geolookup/conditions/q/CA/San_Jose.json';
 
+
+ $.ajax({
+        url: url,
+        dataType: "jsonp",
+        success: function(data)
+        {
+        	console.log(data);
+            alert('worked');
+        }
+    });
